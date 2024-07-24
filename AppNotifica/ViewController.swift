fce07b3149156d8a14021d336aaa9c0b138f30c2
Jonathan Orsano <Orsanor@gmail.com>
2024-07-24 10:42:25 -0300

    Reapply "Initial Commit"
    This reverts commit 110119bb19be021afb0aa0a9fd9df12ca7b84a73.

---
+++ AppNotifica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNotifica
+//
+//  Created by IFBIOTIC14 on 24/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
