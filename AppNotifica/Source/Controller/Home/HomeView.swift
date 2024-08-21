44e72b1064e340c96a70ab84d3fa8104dda4e732
Jonathan Orsano <74941171+orsanor@users.noreply.github.com>
2024-08-20 22:43:14 -0300

    Criando TableView para registro das ocorrências
    video 14.1

--- AppNotifica/Source/Controller/Home/HomeView.swift
+++ AppNotifica/Source/Controller/Home/HomeView.swift
@@ -18,10 +18,43 @@ class HomeView: ViewDefault {
         super.init(frame: .zero)
     }
     
+    lazy var tableView: UITableView = {
+        let view = UITableView(frame: .zero, style: .grouped)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    } ()
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     override func setupVisualElements() {
+        super.setupVisualElements()
+        addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+        setupTableView()
+    }
+    
+    func setupTableView(){
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellIndentifier")
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension HomeView: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIndentifier", for: indexPath)
+        cell.textLabel?.text = "Ocorrência 1"
+        return cell
     }
 }
